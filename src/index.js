7da5e6866db0bf209e13e68ef16444bb1e33b181
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-03-03 00:15:32 +0100

    Start from the scratch. Again.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import App from "./containers/App";
-import reducer from "./reducers";
-import "./index.css";
-import WebFontLoader from "webfontloader";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-WebFontLoader.load({
-  google: {
-    families: ["Roboto:300,400,500,700", "Material Icons"]
-  }
-});
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-console.log(reducer);
-render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
