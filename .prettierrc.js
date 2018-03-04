3917b7d8b32df966807500054d52caf6101be7dc
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-03-04 08:46:13 +0100

    fixed "single quote"

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 // .prettierrc.js
 module.exports = {
   // Set the default
-  "editor.formatOnSave": true,
+  // "editor.formatOnSave": true,
   // Enable per-language
   // "[javascript]": {
   //     "editor.formatOnSave": true
