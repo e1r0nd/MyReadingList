ae3599f7d6ce277a03087e01f0eba4988078e286
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-03-05 21:35:39 +0100

    added Menu component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,23 @@
 import React from "react";
 // import React, { Component } from "react";
+import { slide as Menu } from "react-burger-menu";
+import "./Navigation.css";
 
-const Navigation = ({ tagline }) => (
-  // class Navigation extends Component {
-  // render() {
-  // return (
-  <header className="top">
-    <h1>Menu</h1>
-    <h3 className="tag-line">
-      <span>{tagline}</span>
-    </h3>
-  </header>
+const Navigation = () => (
+  <Menu>
+    <a id="home" className="menu-item" href="/">
+      Home
+    </a>
+    <a id="about" className="menu-item" href="/about">
+      About
+    </a>
+    <a id="contact" className="menu-item" href="/contact">
+      Contact
+    </a>
+    <a onClick={this.showSettings} className="menu-item--small" href="">
+      Settings
+    </a>
+  </Menu>
 );
-// }
-// }
 
 export default Navigation;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,22 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ListItems from "./ListItems/ListItems";
 import App from "./App/App";
 import NotFound from "./NotFound";
+import Navigation from "../components/Navigation/Navigation";
 
 class Router extends Component {
   state = {
     items: []
   };
+
+  showSettings(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ListItems} />
-          <Route path="/page/:pageId" component={App} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <Fragment>
+        <Navigation />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ListItems} />
+            <Route path="/page/:pageId" component={App} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Fragment>
     );
   }
 }
