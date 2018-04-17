a93b162ac646cb1832067a2d9505822dde60def1
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-04-17 03:41:30 +0200

    fixed List's styles

--- src/components/ListItems/Item.js
+++ src/components/ListItems/Item.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { MyContext } from "../Provider";
+import star from "./star.svg";
 
 class Item extends Component {
   static propTypes = {
@@ -22,6 +23,7 @@ class Item extends Component {
 
   render() {
     const { author, date, mark, order, title, quote } = this.props.book;
+    const starIcon = `<img class="book-mark__itm" src=${star} alt="" />`;
 
     return (
       <MyContext.Consumer>
@@ -33,9 +35,17 @@ class Item extends Component {
             }}
           >
             <span className="list-item__title">&laquo;{title}&raquo;</span>.{" "}
-            <span className="list-item__author">{author}</span>{" "}
-            <span>{mark}</span> <p className="list-item__date">{date}</p>{" "}
-            <span>{order}</span> <p>{quote}</p>
+            <div className="list-item__mark">
+              <span className="list-item__author">{author}</span>{" "}
+              {"Done" === ctx.state.title && (
+                <span
+                  className="book-mark"
+                  dangerouslySetInnerHTML={{ __html: starIcon.repeat(mark) }}
+                />
+              )}
+            </div>
+            <p className="list-item__date">{date}</p> <span>{order}</span>{" "}
+            <p>{quote}</p>
           </li>
         )}
       </MyContext.Consumer>
